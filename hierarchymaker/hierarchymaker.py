3553c861d1f02d8712033aa8262659e3a81d746d
Xenon54Xe <98188367+Xenon54Xe@users.noreply.github.com>
2024-01-28 20:09:47 +0100

    Mise a jour HierarchyMaker
    Amélioration du système de filtre

--- hierarchymaker/hierarchymaker.py
+++ hierarchymaker/hierarchymaker.py
@@ -29,30 +29,34 @@ class HierarchyMaker:
         self.hierarchy_list = ["folder", "root", []]
         make_hierarchy_list(source_path, self.hierarchy_list)
 
-    def get_hierarchy_list(self, extension_allowed: list = None, file_name_allowed: list = None) -> list:
+    def get_hierarchy_list(self, filter_dict: dict = None) -> list:
         """
-        Return a copy of the hierarchy list filtered or not
+        Return a copy of the hierarchy list filtered or not, the filters need to be a dictionary like this:
+
+        {
+        extension: (white_list: True or False, filters: []),
+        file_name: (white_list: True or False, filters: []),
+        folder_name: (white_list: True or False, filters: [])
+        }
         """
-        if extension_allowed is None and file_name_allowed is None:
+        if filter_dict is None:
             return self.hierarchy_list.copy()
-        if extension_allowed is not None and file_name_allowed is not None:
-            raise ("Les filtres d'extension et de nom ne sont pas compatibles,"
-                   "il faut choisir soit l'un soit l'autre")
 
         hierarchy_list_copy = self.hierarchy_list.copy()
 
-        if extension_allowed is not None:
-            extension_allowed = [e.replace(".", "") for e in extension_allowed]
-            clean_hierarchy_list_extension(hierarchy_list_copy, extension_allowed)
-        else:
-            clean_hierarchy_list_file_name(hierarchy_list_copy, file_name_allowed)
+        for filter_type in filter_dict.keys():
+            white_list, filters = filter_dict[filter_type]
+            if filter_type == "extension":
+                filters = [e.replace(".", "") for e in filters]
+            clean_hierarchy_list(hierarchy_list_copy, filter_type, filters, white_list)
+
         return hierarchy_list_copy
 
-    def get_hierarchy_normalised_text(self, extension_allowed: list = None, file_name_allowed: list = None) -> str:
+    def get_hierarchy_normalised_text(self, filter_dict: dict = None) -> str:
         """
         Return a text representing the folder's hierarchy, filtered or not
         """
-        new_hierarchy_list = self.get_hierarchy_list(extension_allowed, file_name_allowed)
+        new_hierarchy_list = self.get_hierarchy_list(filter_dict)
         self.make_hierarchy_normalised_text(new_hierarchy_list)
         return self.hierarchy_normalised_text
 
@@ -90,10 +94,10 @@ def make_hierarchy_list(source_path: str, root: list):
             root[-1].append(["file", file_name])
 
 
-def clean_hierarchy_list_extension(root: list, extension_allowed: list = None):
+def clean_hierarchy_list(root: list, filter_type: str, filters: list, white_list: bool = True):
     """
-    Clean the root using file extension
-    :root: [list, name, [[file, name], [list, name, []], [file, name]]]
+    Clean the root using filters
+    :root: hierarchy-normalised list
     """
     i = 0
     while i < len(root[-1]):
@@ -103,23 +107,35 @@ def clean_hierarchy_list_extension(root: list, extension_allowed: list = None):
         root[-1][i][0] = type du i-ème fichier
         """
         typ = root[-1][i][0]
-        if typ == "file":
-            typ, file_name = root[-1][i]
-            extension = file_name.split(".")[-1]
-            if extension not in extension_allowed:
-                root[-1].pop(i)
-                i -= 1
-        else:
-            clean_hierarchy_list_extension(root[-1][i], extension_allowed)  # list in the i-ème element
-            if len(root[-1][i][-1]) == 0:
-                root[-1].pop(i)
-                i -= 1
+        if filter_type in ["extension", "file_name"]:
+            if typ == "file":
+                typ, file_name = root[-1][i]
+                extension = file_name.split(".")[-1]
+                if filter_type == "extension" and (white_list and extension not in filters or
+                                                   not white_list and extension in filters)\
+                        or filter_type == "file_name" and (white_list and file_name not in filters or
+                                                           not white_list and file_name in filters):
+                    root[-1].pop(i)
+                    i -= 1
+            else:
+                clean_hierarchy_list(root[-1][i], filter_type, filters, white_list)  # list in the i-ème element
+                if len(root[-1][i][-1]) == 0:
+                    root[-1].pop(i)
+                    i -= 1
+        elif filter_type == "folder_name":
+            if typ == "folder":
+                typ, folder_name, lst = root[-1][i]
+                if white_list and folder_name not in filters or not white_list and folder_name in filters:
+                    root[-1].pop(i)
+                    i -= 1
+                else:
+                    clean_hierarchy_list(root[-1][i], filter_type, filters, white_list)  # list in the i-ème element
         i += 1
 
 
-def clean_hierarchy_list_file_name(root: list, file_name_allowed: list = None):
+def clean_hierarchy_list_folder_name(root: list, folder_name_list: list, white_list: bool = False):
     """
-    Clean the root using file name
+    Clean the root using folder name
     :root: [list, name, [[file, name], [list, name, []], [file, name]]]
     """
     i = 0
@@ -130,16 +146,13 @@ def clean_hierarchy_list_file_name(root: list, file_name_allowed: list = None):
         root[-1][i][0] = type du i-ème fichier
         """
         typ = root[-1][i][0]
-        if typ == "file":
-            typ, file_name = root[-1][i]
-            if file_name not in file_name_allowed:
+        if typ == "folder":
+            typ, folder_name, lst = root[-1][i]
+            if white_list and folder_name not in folder_name_list or not white_list and folder_name in folder_name_list:
                 root[-1].pop(i)
                 i -= 1
-        else:
-            clean_hierarchy_list_file_name(root[-1][i], file_name_allowed)  # list in the i-ème element
-            if len(root[-1][i][-1]) == 0:
-                root[-1].pop(i)
-                i -= 1
+            else:
+                clean_hierarchy_list_folder_name(root[-1][i], folder_name_list)  # list in the i-ème element
         i += 1
 
 
@@ -178,13 +191,17 @@ def sort_file_and_folder(lib: list, path: str):
         i += 1
         time += 1
 
-
 """
 source = os.path.abspath("../..")
 h = HierarchyMaker()
 
 h.update(source)
-x = h.get_hierarchy_normalised_text()
+filter_dict = {
+    "extension": (False, []),
+    "file_name": (True, ["main"]),
+    "folder_name": (False, [])
+}
+x = h.get_hierarchy_normalised_text(filter_dict)
 
 with open("hierarchy.txt", "w", encoding="utf-8") as file:
     file.write(x)
